d60fa3f9e3930bd03a4b174612df1074caac64a6
HelenEmmett <helen.emmett@gmail.com>
2024-06-12 19:10:47 +1200

    Begun adding functionality

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,24 +1,53 @@
 const theme1 = document.getElementById("theme1");
 const theme2 = document.getElementById("theme2");
 const theme3 = document.getElementById("theme3");
+const keys = document.querySelectorAll(".keys");
+const deleteKey = document.getElementById("del");
+const resetKey = document.getElementById("reset");
+const equalsKey = document.getElementById("equals");
+let screen = document.getElementById("calc-screen");
 
-theme1.addEventListener("click", setTheme1);
-theme2.addEventListener("click", setTheme2);
-theme3.addEventListener("click", setTheme3);
+// add event listeners to keys
+keys.forEach(key => key.addEventListener('click', () => {
+    addValues(key.value);
+}));
 
-function setTheme1() {
-    document.querySelector("body").classList = "theme1";
-    localStorage.setItem("colorMode", "theme1");
-};
+function addValues(key) {
+    if (screen.innerText === "0") {
+        screen.innerText = key;
+    } else {
+        screen.innerText += key;
+    }
+}
 
-function setTheme2() {
-    document.querySelector("body").classList = "theme2";
-    localStorage.setItem("colorMode", "theme2");
-};
+// delete key functionality
+deleteKey.addEventListener("click", () => {
+    if (screen.innerText.length === 1) {
+        screen.innerText = "0";
+    } 
+    if (screen.innerText !== "0") {
+        screen.innerText = screen.innerText.slice(0, -1);
+    } 
+})
 
-function setTheme3() {
-    document.querySelector("body").classList = "theme3";
-    localStorage.setItem("colorMode", "theme3");
+// reset key functionality
+resetKey.addEventListener("click", () => {
+    screen.innerText = "0";
+})
+  
+// evaluate equation
+equalsKey.addEventListener("click", () => {
+    screen.innerText = eval(screen.innerText);
+})
+
+// theme switcher functionality
+theme1.addEventListener("click", () => { setTheme(theme1.value) });
+theme2.addEventListener("click", () => { setTheme(theme2.value) });
+theme3.addEventListener("click", () => { setTheme(theme3.value) });
+
+function setTheme(theme) {
+    document.querySelector("body").classList = theme;
+    localStorage.setItem("colorMode", theme);
 };
 
 // get colour from local storage
@@ -45,6 +74,5 @@ function switchMode() {
     }
 };
 
-
-// check if user preference is set to light mode, and adjust checkbox
+// check if user preference is set to light mode, and adjust checkbox and theme
 switchMode();
